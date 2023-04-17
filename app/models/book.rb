fbf7bb2ea111ab43145f1620cdabf5579ef831af
fillson22 <fillson22051983@gmail.com>
2023-04-17 23:01:36 +0300

    add model controller and views

--- app/models/book.rb
+++
@@ -1,4 +0,0 @@
-class Book < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true 
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
-  root "home#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :books
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
