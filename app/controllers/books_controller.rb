b9b7ca17da837b725b0e26faf04d939a2347cafe
fillson22 <fillson22051983@gmail.com>
2023-06-07 22:07:54 +0300

    111

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show]
 
   def index
     @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   private
 
   def set_book
     @book = Book.find(params[:id])
-    # if @book 
   end
 end
