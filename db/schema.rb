c427aba535a39c9cbf8cadcebb6ef76affe7ed49
fillson22 <fillson22051983@gmail.com>
2023-09-18 10:57:13 +0300

    add :references beetwin book and comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_080518) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_075554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_080518) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "book_id", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,4 +76,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_080518) do
   end
 
   add_foreign_key "books", "categories"
+  add_foreign_key "comments", "books"
 end
