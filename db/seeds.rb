1059a8955d644276eea893ee025a5c56dcdb5447
fillson22 <fillson22051983@gmail.com>
2023-09-18 14:18:37 +0300

    Upgrate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,15 @@ Category.create( name: 'Mystery' )
 
 Book.create( name: Faker::Book.title, 
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada elementum volutpat. Nulla euismod, nunc.', 
-    category_id: '14' )
+    category_id: '1' )
 Book.create( name: Faker::Book.title, 
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada elementum volutpat. Nulla euismod, nunc.', 
-    category_id: '15' )
+    category_id: '2' )
 Book.create( name: Faker::Book.title, 
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada elementum volutpat. Nulla euismod, nunc.', 
-    category_id: '16' )
+    category_id: '3' )
+3.times do
+    Comment.create(body: Faker::Games::WorldOfWarcraft.quote, book_id: '1')
+end
 
-#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
