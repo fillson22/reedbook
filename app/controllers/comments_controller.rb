3cb32ffdca58e767c83c99d82d0bced4ced28677
fillson22 <fillson22051983@gmail.com>
2023-09-20 16:48:49 +0300

    make views comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,19 @@
 class CommentsController < ApplicationController
+
+  def create
+    @book = Book.find_by id: params[:book_id]
+    @comment = @book.comments.new(comment_params)
+    if @comment.save 
+        redirect_to book_path 
+      else 
+        flash[:success] = 'Don`t create!'
+      end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
     
   end
