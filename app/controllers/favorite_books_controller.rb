232ffb954ec4f62bedfb22b939ca6bafe763aab8
fillson22 <fillson22051983@gmail.com>
2023-10-23 16:54:21 +0300

    hren

--- app/controllers/favorite_books_controller.rb
+++ app/controllers/favorite_books_controller.rb
@@ -6,8 +6,19 @@ class FavoriteBooksController < ApplicationController
   end
 
   def create
+    @favorite_book = current_user&.favorite_books&.build(favo_create_params)
+    if @favorite_book&.save
+      render 'books/index'
+    else
+      render 'home/index'
+    end
+
   end
 
+  private
 
+  def favo_create_params
+    params.require(:favorite_book).permit(:book_id)
+  end
 
 end
