90d7309157674a6cf8f3a7a0d1ff95e34a0db99f
fillson22 <fillson22051983@gmail.com>
2023-10-27 15:02:58 +0300

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,62 @@ book = Book.create(name: 'Мавка. Лісова пісня',
 book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
                   filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
 
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+            добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+                          добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+                          добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+                  filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+            добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+                          добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+                          добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+                          добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+
+book = Book.create(name: 'Мавка. Лісова пісня',
+            description: 'Цей твір розповідає про зв`язок людини i природи, про безсмертя вічних людських цінностей, 
+                          добро i зло; про пошук гармонії, кохання i зраду; про матеріальне i духовне в житті.',
+            category_id: 4 )
+book.image.attach(io: URI.open("https://book-ye.com.ua/upload/resize_cache/iblock/684/520_860_1/fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg"),
+            filename: "fd8f9b78_d214_11ed_8182_00505684ea69_63af60eb_d216_11ed_8182_00505684ea69.jpg")
+            
 # Book.create(name: Faker::Book.title,
 #             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 #             Integer malesuada elementum volutpat. Nulla euismod, nunc.',
